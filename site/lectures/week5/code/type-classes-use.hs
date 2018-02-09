a09345170aee8e343a3ab2abb491ebbc229d4635
Deian Stefan <deian@intrinsic.com>
2018-02-08 18:17:40 -0800

    update site for publish

--- site/lectures/week5/code/type-classes-use.hs
+++ site/lectures/week5/code/type-classes-use.hs
@@ -3,29 +3,10 @@ module HasMap where
 
 import Prelude hiding (map)
 
--- map :: (a -> b) -> [a] -> [b]
-mapMaybe :: (a -> b) -> Maybe a -> Maybe b
-mapMaybe f Nothing = Nothing
-mapMaybe f (Just x) = Just (f x)
+-- allEqual'  :: ???
+allEqual' a b c = a == b && b == c 
 
-data Tree a = Leaf a
-            | Node (Tree a) (Tree a)
+-- Reall: map :: (a -> b) -> [a] -> [b]
+-- Let's define this function to work on arbitrary things and not just lists,
+-- say Maybe types and Tree types.
 
-mapTree :: (a -> b) -> Tree a -> Tree b
-mapTree f (Leaf x) = Leaf (f x)
-mapTree f (Node t1 t2) = Node (mapTree f t1) (mapTree f t2)
-
-class HasMap g where
-    map :: (a -> b) -> g a -> g b
-
-instance HasMap [] where
-    map :: (a -> b) -> [a] -> [b]
-    map f [] = []
-    map f (x:xs) = f x : map f xs
-
-
-instance HasMap Maybe where
-    map = mapMaybe
-
-instance HasMap Tree where
-    map = mapTree
